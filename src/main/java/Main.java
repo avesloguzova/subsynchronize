f4c5be650f8026050d4488a144768a2a29c42852
Dmitry Tishchenko <flir3d@gmail.com>
2015-05-05 17:31:20 +0300

    An example of using Sphinx SpeechAligner.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,33 @@
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.SpeechAligner;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by Dmitry Tishchenko on 05.05.15.
  */
 public class Main {
     public static void main(String[] args) {
 
+        Configuration configuration = new Configuration();
+
+        String audioPath = args[1];
+        String scriptPath = args[2];
+
+        // Set path to acoustic model.
+        String amPath = "resource:/edu/cmu/sphinx/models/en-us/en-us";
+        // Set path to dictionary.
+        String dictPath = "resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict";
+        // Set language model.
+        configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.dmp");
+
+        try {
+            SpeechAligner aligner = new SpeechAligner(amPath, dictPath, null);
+            aligner.align(new URL(audioPath), "one oh one four two");
+        } catch(IOException e) {
+            System.out.print(e.getMessage());
+        }
     }
 }
